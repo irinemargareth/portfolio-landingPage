0b87fc048f5901ca9d6d63f52f1df622573c0a6d
Irine Margareth <irine.margareth@gmail.com>
2025-09-10 15:19:36 +0700

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,84 @@
+/*// keep track of previous scroll position
+let prevScrollPos = window.scrollY;
+
+window.addEventListener('scroll', function() {
+  // current scroll position
+  const currentScrollPos = window.scrollY;
+
+  if (prevScrollPos > currentScrollPos) {
+    // user has scrolled up
+    document.querySelector('.navbar').classList.add('show');
+  } else {
+    // user has scrolled down
+    document.querySelector('.navbar').classList.remove('show');
+  }
+
+  // update previous scroll position
+  prevScrollPos = currentScrollPos;
+}); 
+
+/* let lastScrollTop = 0;
+  navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function(){
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.classList.add('active');
+  } else {
+    navbar.classList.remove('active');
+  }
+  lastScrollTop = scrollTop;
+}) */
+
+/*var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}*/
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var grid = document.getElementById("image-grid");
+var btns = grid.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
